33761130263a0631cbbdc82cebe34b13e61f2660
Tajini Punchihewa <tajini94@gmail.com>
2023-10-26 17:49:25 -0700

    investment

---
+++ app/src/main/java/com/example/appdevproject/Investment/NewInvestmentActivity.java
@@ -0,0 +1,48 @@
+package com.example.appdevproject.Investment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.appdevproject.R;
+
+public class NewInvestmentActivity extends AppCompatActivity {
+
+    Button saveBtn;
+    EditText iname,itype,iamount;
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_investment);
+
+        saveBtn = findViewById(R.id.btnInvesSave);
+        iname = findViewById(R.id.InvesName);
+        itype = findViewById(R.id.InvesType);
+        iamount = findViewById(R.id.InvesAmount);
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userId = String.valueOf(1);
+                String name = iname.getText().toString()+"\n";
+                String type = itype.getText().toString();
+                String amount = iamount.getText().toString();
+
+                DbHandlerInvestment dbHandler = new DbHandlerInvestment(NewInvestmentActivity.this);
+
+                dbHandler.insertInvestments(userId, name,type,amount);
+                Toast.makeText(getApplicationContext(), "New Investment Added Successfully!",Toast.LENGTH_LONG).show();
+                intent = new Intent(NewInvestmentActivity.this, InvestmentActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/appdevproject/LandingPage/IntroActivity.java
+++ app/src/main/java/com/example/appdevproject/LandingPage/IntroActivity.java
@@ -2,15 +2,51 @@ package com.example.appdevproject.LandingPage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.appdevproject.Budget.Budget;
+import com.example.appdevproject.Investment.InvestmentActivity;
+import com.example.appdevproject.Loans.Loans;
 import com.example.appdevproject.R;
+import com.example.appdevproject.User.EditUserPage;
 
 public class IntroActivity extends AppCompatActivity {
 
+    TextView txtBudget;
+    TextView txtDebt;
+    TextView txtInvestment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        txtBudget = findViewById(R.id.txtBudget);
+        txtDebt = findViewById(R.id.txtDebt);
+        txtInvestment = findViewById(R.id.txtInvestment);
+
+        txtBudget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroActivity.this, Budget.class));
+            }
+        });
+
+        txtDebt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroActivity.this, Loans.class));
+            }
+        });
+        txtInvestment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroActivity.this, InvestmentActivity.class));
+            }
+        });
     }
 }
